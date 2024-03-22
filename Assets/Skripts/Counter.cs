73c9b02477f9a9592e8c1b1019e06eab5166c814
bbshkins <kostyapk3@gmail.com>
2024-03-23 00:22:21 +0300

    Added Counter Skript

---
+++ Assets/Skripts/Counter.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private float _delay;
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private float _currentNumber = 0f;
+    private float _step = 1f;
+    private Coroutine _coroutine;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            SelectAction();
+    }
+
+    private void SelectAction()
+    {
+        if (_coroutine == null)
+            Restart();
+        else
+            Stop();
+    }
+
+    private void Restart()
+    {
+        _coroutine = StartCoroutine(Count());
+    }
+
+    private void Stop()
+    {
+        StopCoroutine(_coroutine);
+        _coroutine = null;
+    }
+
+    private IEnumerator Count()
+    {
+        var delay = new WaitForSeconds(_delay);
+        bool isWorking = true;
+
+        while (isWorking)
+        {
+            _currentNumber += _step;
+            _text.text = _currentNumber.ToString();
+
+            yield return delay;
+        }
+    }
+}
